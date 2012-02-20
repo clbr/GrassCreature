fcadd71b02907730f377f816b272e1c03b49e3ec
kamilla <joonas.greis@kamillaproductions.com>
2012-02-20 15:42:17 +0200

    modified

--- DatabaseOperation/idea.php
+++ DatabaseOperation/idea.php
@@ -1,21 +1,23 @@
 <?php
 
-function addIdea() {
-
 $con = mysql_connect("mysql.labranet.jamk.fi","ideapankki","0jWF)(p35j%J");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
+  echo "FAIL";
   }
 
-mysql_select_db("my_db", $con);
+mysql_select_db("ideapankki_dev", $con);
+$query = mysql_query('SELECT * FROM Idea');
+$results = mysql_fetch_assoc($query);
 
+echo $query;
+echo 'The query returned ' . $results['Name'];
 
-mysql_query("INSERT INTO Idea (Name, Description, Version, Status, RequestDate, Inventor) VALUES ('undefined', 'undefined', 0, 'new', CURDATE(), 0);");
-
+mysql_query("INSERT INTO Idea (Name, Description, Version, Status, RequestDate, Inventor) VALUES ('undefined', 'undefined', 0, 'new', CURDATE(), 0)");
+echo "ADDED";
 
 mysql_close($con);
 
-}
 
 ?>

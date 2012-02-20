b13ebddf5300302c4aa044ccdd24cd0fe8c998cd
Lauri Kasanen <cand@gmx.com>
2012-02-20 12:01:47 +0000

    Start session handling

--- session.php
+++ session.php
@@ -2,10 +2,29 @@
 
 session_start();
 
+class sess {
 
+	function isLoggedIn() {
 
+		if (isset($_SESSION["userID"]))
+			return true;
+		else
+			return false;
+	}
 
+	function isAdmin() {
 
+		if (isset($_SESSION["isAdmin"]))
+			return true;
+		else
+			return false;
+	}
+}
+
+$sess = new sess;
+
+if ($sess->isLoggedIn()) echo "logged in";
+else echo "logged out";
 
 
 

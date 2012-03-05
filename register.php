e69ba4bc8dabf1f3873a77a4edb3243b5aaeeea6
f6133 <f6133@jamk.fi>
2012-03-05 10:56:28 +0200

    Copy template to register.php

---
+++ register.php
@@ -0,0 +1,21 @@
+<?php require_once("session.php"); ?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+  "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>Ideabank</title>
+
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+</head>
+
+<body>
+
+
+
+<script src=js/js.js></script>
+</body>
+</html>

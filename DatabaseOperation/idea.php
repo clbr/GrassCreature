03fe1da1fbf93ec253433df79da72d3fac98087a
f6133 <f6133@jamk.fi>
2012-03-05 12:05:37 +0200

    Restore idea.php to working order

--- DatabaseOperation/idea.php
+++ DatabaseOperation/idea.php
@@ -1,13 +1,22 @@
-<?php	
-	require_once("details.php");
+<?php
 
 require_once("details.php");
 
 function addIdea($name, $desc) {
 
-$mysqli = db_connect();
-	function addComment() {
-		$mysqli = db_connect();
+	$mysqli = db_connect();
+
+	$sql = "INSERT INTO Idea (Name, Description, Version, Status, RequestDate, Inventor) VALUES ('$name', '$desc', 0, 'new', CURDATE(), 1)";
+	$result = $mysqli->query($sql);
+
+	if(!$result) {
+		$mysqli->close();
+		return 1;
 	}
+
+	$mysqli->close();
+	return 0;
+
+}
 
 ?>

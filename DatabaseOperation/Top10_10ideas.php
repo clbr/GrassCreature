04d01e7cc62b72a3ecec27a1c014db89272be44b
f7197 <samu_tynkkynen@hotmail.com>
2012-03-05 15:43:18 +0200

    ei niin valmis vielakaan

--- DatabaseOperation/Top10_10ideas.php
+++ DatabaseOperation/Top10_10ideas.php
@@ -1,39 +1,33 @@
-<html>
-<head><title>Top101</title></head>
-<body>
 
-<h1></h1>
 <?php
-require_once("session.php");
-require_once("details.php"); 
-function top101(){
-$yhteys = functiondb_connect(); 
-
-
-$sql = "SELECT * FROM Idea ORDER BY AddingDate DESC";
-$result = $mysqli->query($sql) or die($mysqli->error);
-if($result){
-echo"<table border='1'>";
-$result->data_seek(0);
-while($row = $result->fetch_row()){
-echo"<tr><td>$row[0]</td>
-<td>$row[1]</td>
-<td>$row[2]</td>
-<td>$row[3]</td>
-<td>$row[4]</td>
-<td>$row[5]</td>
-<td>$row[6]</td>
-<td>$row[7]</td>
-<td>$row[8]</td>
-<td>$row[9]</td><td>",$row[]+$row[]+$row[]+$row[]+$row[]+$row[]+$row[]+$row[]+$row[]+$row[],"</td>
-</tr>\n";
-}
-echo "</table>";
-$result->close();
-}
-else{echo "Error";}
-$yhteys->close();
+function top101()
+{
+	require_once("S:\www\details.php"); 
+	$yhteys = functiondb_connect(); 
+	$sql = "SELECT * FROM Idea ORDER BY AddingDate DESC";
+	$result = $mysqli->query($sql) or die($mysqli->error);
+	if($result)
+	{
+		echo"<table border='1'>";
+		$result->data_seek(0);
+		while($row = $result->fetch_row())
+		{
+			echo"<tr><td>$row[0]</td>
+			<td>$row[1]</td>
+			<td>$row[2]</td>
+			<td>$row[3]</td>
+			<td>$row[4]</td>
+			<td>$row[5]</td>
+			<td>$row[6]</td>
+			<td>$row[7]</td>
+			<td>$row[8]</td>
+			<td>$row[9]</td><td>",$row[0]+$row[1]+$row[2]+$row[3]+$row[4]+$row[5]+$row[6]+$row[7]+$row[8]+$row[9],"</td>
+			</tr>\n";
+		}
+		echo "</table>";
+		$result->close();
+	}
+	else{echo "Error";}
+	$yhteys->close();
 }
 ?>
-</body>
-</html>

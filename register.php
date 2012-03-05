a492dff3a6017b2b5b5460daa281e192b4584129
f6133 <f6133@jamk.fi>
2012-03-05 11:39:52 +0200

    Create and require the db side of registration

--- register.php
+++ register.php
@@ -1,4 +1,9 @@
-<?php require_once("session.php"); ?>
+<?php
+
+require_once("session.php");
+require_once("DatabaseOperation/register.php");
+
+?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
   "http://www.w3.org/TR/html4/loose.dtd">

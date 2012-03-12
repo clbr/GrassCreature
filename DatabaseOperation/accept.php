60ee7a10266d0dcc39aaa96f93d89aa0e5ea5fce
f6133 <f6133@jamk.fi>
2012-03-12 14:26:52 +0200

    Add a countNewIdeas function

--- DatabaseOperation/accept.php
+++ DatabaseOperation/accept.php
@@ -25,4 +25,25 @@ return $sth;
 
 }
 
+// How many new ideas?
+function countNewIdeas() {
+
+	$db = db_connect();
+
+	$st = $db->prepare("select Name from Idea where Status = 'new'");
+	if (!$st)
+		die($db->error);
+
+	if (!$st->execute())
+		die($db->error);
+
+	$st->store_result();
+
+	$num = $st->num_rows;
+
+	$db->close();
+
+	return $num;
+}
+
 ?>

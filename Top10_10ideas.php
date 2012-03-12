e5bc5771e94b3327937804b5d23eb1b89db8abcc
f7197 <samu_tynkkynen@hotmail.com>
2012-03-12 15:57:42 +0200

    uudempi versio

--- Top10_10ideas.php
+++ Top10_10ideas.php
@@ -1,37 +1,45 @@
 
-<?php
-error_reporting(E ALL);
-function top101()
+
+<?
+error_reporting(E_ALL);
+function top102()
 {
 //Connection to db and info request
-	require_once("S:\www\details.php");
-	$yhteys = functiondb_connect();
-	$sql = "SELECT Name, Inventor FROM Idea ORDER BY AddingDate DESC";
+	require_once("details.php");
+	$mysqli = functiondb_connect();
+	if(mysqli_connect_errno())
+	{
+	echo "Yhteyttä tietokantaan ei saatu: ";
+	echo mysqli_connect_error();
+	}
+	$sql = "SELECT Name, Inventor, AddingDate FROM Idea ORDER BY AddingDate DESC";
 	$result = $mysqli->query($sql) or die($mysqli->error);
 	if($result)
 	{
 	//table creation and data insertion
 		echo"<table border='1'>";
 		$result->data_seek(0);
-		while($row = $result->fetch_row())
+		echo "<tr><td>Idea Name</td><td>Inventor</td><td>Time when added</td></tr>";
+		
+		for($i=0; $i<10; $i++)
 		{
-			echo"<tr><td>$row[0]</td>
+		$row = $result->fetch_row();
+			echo"<tr>
+			<td>$row[0]</td>
 			<td>$row[1]</td>
 			<td>$row[2]</td>
-			<td>$row[3]</td>
-			<td>$row[4]</td>
-			<td>$row[5]</td>
-			<td>$row[6]</td>
-			<td>$row[7]</td>
-			<td>$row[8]</td>
-			<td>$row[9]</td><td>",$row[0]+$row[1]+$row[2]+$row[3]+$row[4]+$row[5]+$row[6]+$row[7]+$row[8]+$row[9],"</td>
-			</tr>\n";
+			
+			</tr>";
+			
+			//echo "<pre>"; var_dump($row); echo "</pre>";
 		}
-		echo "</table>";
+		echo"</table>";
 		$result->close();
 	}
 	else{echo "Error";}
 	//disconnect
-	$yhteys->close();
+	$mysqli->close();
 }
 ?>
+
+

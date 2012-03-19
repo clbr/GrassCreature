a57eeef5a5dfdec6712350a1c8fbf806f96e90df
Lauri Kasanen <cand@gmx.com>
2012-03-19 12:06:34 +0200

    Better formatting of top10

--- Top10_10ideas.php
+++ Top10_10ideas.php
@@ -1,11 +1,13 @@
 <?php
 error_reporting(E_ALL);
+
+// top 10 newest ideas
 function top102()
 {
 //Connection to db and info request
 	require_once("DatabaseOperation/details.php");
-	$mysqli=db_connect();
-	$sql = "SELECT Name, Inventor, AddingDate FROM Idea ORDER BY AddingDate DESC limit 10";
+	$mysqli = db_connect();
+	$sql = "SELECT Name, IdeaID, AddingDate FROM Idea ORDER BY AddingDate DESC limit 10";
 	$result = $mysqli->query($sql) or die($mysqli->error);
 	if($result)
 	{
@@ -16,8 +18,7 @@ function top102()
 		while($i<10)
 		{
 		$row = $result->fetch_row();
-			echo"<tr><td>$row[0]</td>
-			<td>$row[1]</td>
+			echo"<tr><td><a href='showIdea.php?id=$row[1]'>$row[0]</a></td>
 			<td>$row[2]</td>
 			</tr>\n";
 			$i++;

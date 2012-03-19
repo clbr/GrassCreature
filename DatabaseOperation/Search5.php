5e12e1eb9585612cd3a976c8c30f8b2e2556e03a
Dracyllion <Dracyllion@hotmail.com>
2012-03-19 14:15:21 +0200

    meniskhn nyt oikein Search

--- DatabaseOperation/Search5.php
+++ DatabaseOperation/Search5.php
@@ -1,39 +1,42 @@
 <?php
-/*error_reporting(E_ALL);*/
+error_reporting(E_ALL);
 require_once("details.php");
 function advancedSearch() {
 
 $mysqli = db_connect();
 
-$date = $_POST['date'];		
-
-$status1 = $_POST['status'];
-$inventor1 = $_POST['inventor'];
-$tag1 = $_POST['tags'];	
-
-// Splitting the $tag1 string into pieces
-
-$pieces = explode(" ", $tag1);
-$count = count($pieces);
+$date = $_POST['date'];	
 
 if($date!=null){	
  print"<table border=1>\n";
  print "<tr><td><strong>Idea name</strong></td><td><strong>Version</strong></td><td><strong>Description</strong></td><td><strong>
  Status</strong></td><td> <strong>RequestDate</strong></td><td><strong>Added On</strong></td><td><strong>Inventor</strong></td>
  </tr>\n";}
+ 
 
-	
+
+$tag1 = $_POST['tags'];		
+	// Splitting the $tag1 string into pieces
+
+$pieces = explode(" ", $tag1);
+$count = count($pieces);
+
+
+
 	foreach($pieces as $keyword)
-	{
+	{	
+		
 	$keyword2 = "%".$keyword."%";
-	
+		 
+						 
 	
 	$sql = "SELECT Name, Version, LEFT(Description, 100), Status, RequestDate, AddingDate, Inventor
 						  FROM Idea
 						  WHERE Status= (?)
-						  AND Inventor LIKE CONCAT('%',(?),'%')
+						   AND Inventor LIKE CONCAT('%',(?),'%')
 						  AND Description LIKE CONCAT('%',(?),'%')
-						  ORDER BY RequestDate ";	
+						  ORDER BY AddingDate ";	
+						  
 
 	if ($date == "Newest")
 		{
@@ -47,16 +50,19 @@ if($date!=null){
 		$stmt = $mysqli->prepare($sql);	
   
 	if (!$stmt) die ("NOOOOOO " . $mysqli->error);
-	
+
 	
 $stmt->bind_param("sss",$status1, $inventor1, $keyword2);
-	
+		
+	$status1 = $_POST['status'];
+$inventor1 = $_POST['inventor'];
+
 $stmt->execute();		
 
 $stmt->bind_result($name, $version, $desc, $stat, $dateReq, $dateAdd, $inventor);
 
 $stmt->store_result();
-			  
+
 
 	while($stmt->fetch())
 	{

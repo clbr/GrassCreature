90e4f1331aef9632635d95243ba4203c088f48b6
Lauri Kasanen <cand@gmx.com>
2012-03-21 13:33:58 +0200

    User-side group admin

--- DatabaseOperation/groups.php
+++ DatabaseOperation/groups.php
@@ -33,6 +33,32 @@ function getGroups($userid, $isadmin) {
 
 	} else {
 
+		$st = $db->prepare("select Name, Description, GroupID from UserGroup, User_has_Group where User_UserID = ? and Group_GroupID = GroupID") or die($db->error);
+
+		$st->bind_param("i", $userid);
+		$st->execute();
+		$st->bind_result($name, $desc, $gid);
+		$st->store_result();
+
+		if ($st->num_rows < 1)
+			return;
+
+		echo "<table border=0 class='highlight center'>\n";
+		echo "<tr><th></th><th>Name</th><th>Description</th><th>Members</th></tr>\n";
+
+		while ($row = $st->fetch_row()) {
+			echo "<tr><td>";
+			echo "<input type=checkbox name='chk[]' value=$row[2]>";
+			echo "</td><td>$row[0]</td><td>$row[1]</td><td>";
+
+			$mem = $db->query("select count(User_UserID) from User_has_Group where Group_GroupID = $row[2]") or die ($db->error);
+			if ($row = $mem->fetch_row()) {
+				echo "$row[0]";
+			}
+			echo "</td></tr>\n";
+		}
+
+		echo "</table>\n";
 	}
 
 

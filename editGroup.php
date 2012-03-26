b42922784ed2b0835383a890167655f88983b349
Lauri Kasanen <cand@gmx.com>
2012-03-26 10:10:50 +0300

    Add initial editGroups

---
+++ editGroup.php
@@ -0,0 +1,41 @@
+<?php require_once("session.php");
+
+if (!isset($_GET["id"])) {
+	echo "<script type=\"text/javascript\">" .
+		"alert(\"No id given\"); window.history.back();" .
+		"</script>";
+	return;
+}
+
+$id = $_GET["id"];
+
+
+require_once("DatabaseOperation/groups.php");
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+  "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>Ideabank</title>
+
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	<base target=main>
+</head>
+
+<body class=lining>
+
+<h2>Editing members of group
+<?php
+
+echo getGroupName($id);
+
+?>
+</h2>
+
+<script src="js/js.js" type="text/javascript"></script>
+</body>
+</html>

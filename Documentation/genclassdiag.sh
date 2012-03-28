94b9342c9d2621469ce38d1bffaecd8021368608
Lauri Kasanen <cand@gmx.com>
2012-03-28 19:26:53 +0300

    Count lines and functions

--- Documentation/genclassdiag.sh
+++ Documentation/genclassdiag.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
+lines=0
+funcs=0
+
 for i in `find -name "*.php" | grep -v pchart`; do
 
 	echo "$i:"
 	grep -e '^[[:space:]]*function' $i
+
+	f=`grep -e '^[[:space:]]*function' $i | wc -l`
+	funcs=$((funcs + f))
+
+	l=`wc -l $i | cut -d" " -f1`
+	lines=$((lines + l))
 done
+
+echo "$lines lines in total (including blanks and comments)"
+echo "$funcs functions"

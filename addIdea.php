bc22800e7a95fb70a35304b27285c880a078abdd
kamilla <joonas.greis@kamillaproductions.com>
2012-04-02 13:21:56 +0300

    fixed htmlspecialcharsbug adding idea

--- addIdea.php
+++ addIdea.php
@@ -56,7 +56,7 @@ $sess->mustBeLoggedIn();
 			        $categoryString="";
 			        $category=getMostUsedCategories();
  			        foreach($category as $value) {
-                			$categoryString=$categoryString."<div class='mostused' onClick='getElementById(\"category\").value+=\"".$value." \"'>".$value."</div>";
+                			$categoryString=$categoryString."<div class='mostused' onClick='getElementById(\"category\").value+=\"".htmlspecialchars($value, ENT_QUOTES)." \"'>".$value."</div>";
         			}
 			echo $categoryString;
 			?><br>

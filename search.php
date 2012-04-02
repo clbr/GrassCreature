3803bc3bab8de736d33ea8e58ad398672faabce9
Lauri Kasanen <cand@gmx.com>
2012-04-02 14:01:03 +0300

    Revert "s"
    This reverts commit 6cb744f2bdac9fc04575894055b4e86dfc4db20e.

--- search.php
+++ search.php
@@ -1,4 +1,4 @@
-﻿<?php require_once("session.php"); ?>
+<?php require_once("session.php"); ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
   "http://www.w3.org/TR/html4/loose.dtd">

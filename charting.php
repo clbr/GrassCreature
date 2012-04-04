bf8a9993293ea58abc08876e34657135ebd818ae
Lauri Kasanen <cand@gmx.com>
2012-04-04 12:14:48 +0300

    Add shorthand include to pchart

---
+++ charting.php
@@ -0,0 +1,9 @@
+<?php
+
+error_reporting(E_ALL);
+
+require_once("pchart/class/pData.class.php");
+require_once("pchart/class/pDraw.class.php");
+require_once("pchart/class/pImage.class.php");
+
+?>

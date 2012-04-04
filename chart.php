5120bf4311ceec1e288c49184c3652a3a027ddbb
Lauri Kasanen <cand@gmx.com>
2012-04-04 12:28:41 +0300

    Replace chart.php with a working example

--- chart.php
+++ chart.php
@@ -1,74 +1,51 @@
 <?php
-//function chart
-//{
-	//standard includes
-	include("pchart/class/pData.class.php");  
-	include("pchart/class/pDraw.class.php");
-	include("pchart/class/pImage.class.php");
-	
-	// creating dataobject
-	$OneData=new pData();
-	
-	//db actions
-	require_once("details.php");
-	$mysqli = db_connect();
-	$sql = "SELECT Distinct DATE_FORMAT(Date, '%d/%m/%Y' ) As vrk, count(CommentID) AS Comments FROM Comment GROUP BY vrk ORDER BY Date desc limit 30";
-	$result = $mysqli->query($sql) or die($mysqli->error);
-	$Date=""; $Count="";
-	while($row=mysql_fetch_array($result))
-	{
-	//get data from query result
-	$Date[]=$row["vrk"];
-	$Count[]=$row["Comments"];
-	}
-	//save data in pData array
-	$OneData->addPoints($Date,"Date");
-	$OneData->addPoints($Count, "Comments");
-	
-	//abscissa axis
-	$OneData->setAbscissa("Date");
-	
-	//associate "Comments" Dataserie to the second axis
-	$Onedata->setSerieOnAxis("Comments", 0);
-	
-	//Name this axis "Time"
-	$OneData->setXAxisName("Date");
-	
-	//Y-axis
-	$Onedata->setAxisName(0, "Comments");
-	$OneData->setAxisUnit(0, "Comments");
-	
-	//Drawing:
-	
-	//pchart object & data association
-	$OneChart = new pImage(700,230,$OneData);
-	
-	//Antialiasing off
-	$OneChart->Antialias = FALSE;
-	
-	//border for picture
-	$OneCHart->drawGradientArea(0,0,700,230,DIRECTION_VERTICAL,array("StartR"=>240,"StartG"=>240,"StartB"=>240,"EndR"=>180,"EndG"=>180,"EndB"=>180,"Alpha"=>100));
-	$OneChart->drawGradientArea(0,0,700,230,DIRECTION_HORIZONTAL,array("StartR"=>240,"StartG"=>240,"StartB"=>240,"EndR"=>180,"EndG"=>180,"EndB"=>180,"Alpha"=>20));
-	$OneChart->drawRectangle(0,0,699,229,array("R"=>0,"G"=>0,"B"=>0));
-	
-	//font
-	$OneChart->setFontProperties(array("FontName"=>"pchart/fonts/GeosansLight.ttf","FontSize"=>12));
-	
-	//Draw scale
-	$scaleSettings = array("GridR"=>200,"GridG"=>200,"GridB"=>200,"DrawSubTicks"=>TRUE,"CycleBackground"=>TRUE);
-	$OneChart->drawScale($scaleSettings);
-	
-	//chart legend
-	$OneChart->drawLegend(580,12,array("Style"=>LEGEND_NOBORDER,"Mode"=>LEGEND_HORIZONTAL));
-	
-	//shadow on
-	$OneChart->setShadow(TRUE,array("X"=>1,"Y"=>1,"R"=>0,"G"=>0,"B"=>0,"Alpha"=>10));
-	
-	//draw chart
-	$OneChart->setShadow(TRUE,array("X"=>1,"Y"=>1,"R"=>0,"G"=>0,"B"=>0,"Alpha"=>10));
-	$settings = array("Surrounding"=>-30,"InnerSurrounding"=>30);
-	$OneChart->drawBarChart($settings);
-	
-	//render
-	$OnceChart->autoOutput("example.drawBarChart.simple.png");
+error_reporting(E_ALL);
+
+/* pChart library inclusions */
+require_once("charting.php");
+
+require_once("DatabaseOperation/details.php");
+
+/* Create and populate the pData object */
+$data = new pData();
+
+$db = db_connect();
+
+$res = $db->query("select count(CommentID), Idea_IdeaID from Comment group by Idea_IdeaID order by count(CommentID) desc") or die($db->error);
+
+while ($row = $res->fetch_row()) {
+	$data->addPoints($row[0], "Comments");
+	$data->addPoints($row[1], "Labels");
+}
+
+$db->close();
+
+$data->setAxisName(0,"Kommentteja");
+$data->setAbscissa("Labels"); // X-akselin otsikot
+
+/* Create the pChart object */
+$myPicture = new pImage(800,230,$data);
+
+/* Draw the background */
+$Settings = array("R"=>170, "G"=>183, "B"=>87, "Dash"=>1, "DashR"=>190, "DashG"=>203, "DashB"=>107);
+$myPicture->drawFilledRectangle(0,0,800,230,$Settings);
+
+/* Overlay with a gradient */
+$Settings = array("StartR"=>219, "StartG"=>231, "StartB"=>139, "EndR"=>1, "EndG"=>138, "EndB"=>68, "Alpha"=>50);
+$myPicture->drawGradientArea(0,0,800,230,DIRECTION_VERTICAL,$Settings);
+$myPicture->drawGradientArea(0,0,800,20,DIRECTION_VERTICAL,array("StartR"=>0,"StartG"=>0,"StartB"=>0,"EndR"=>50,"EndG"=>50,"EndB"=>50,"Alpha"=>80));
+
+/* Write the chart title */
+$myPicture->drawText(250,55,"Kommentteja per idea",array("FontSize"=>20,"Align"=>TEXT_ALIGN_BOTTOMMIDDLE));
+
+/* Draw the scale and the 1st chart */
+$myPicture->setGraphArea(60,60,750,190);
+$myPicture->drawFilledRectangle(60,60,750,190,array("R"=>255,"G"=>255,"B"=>255,"Surrounding"=>-200,"Alpha"=>10));
+$myPicture->drawScale(array("DrawSubTicks"=>TRUE));
+$myPicture->setShadow(TRUE,array("X"=>1,"Y"=>1,"R"=>0,"G"=>0,"B"=>0,"Alpha"=>10));
+$myPicture->drawBarChart(array("DisplayValues"=>TRUE,"DisplayColor"=>DISPLAY_AUTO,"Rounded"=>TRUE,"Surrounding"=>30));
+$myPicture->setShadow(FALSE);
+
+/* Render the picture (choose the best way) */
+$myPicture->stroke();
 ?>

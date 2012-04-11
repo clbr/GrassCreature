4d5ab9b81fd610b0d955bee7d6dd87548227788a
Lauri Kasanen <cand@gmx.com>
2012-04-11 13:54:49 +0300

    Smoother lines

--- chart.php
+++ chart.php
@@ -24,7 +24,7 @@ $pic->setGraphArea(60,60,830,190);
 $pic->drawFilledRectangle(60,60,830,190,array("R"=>255,"G"=>255,"B"=>255,"Surrounding"=>-200,"Alpha"=>10));
 $pic->drawScale(array("DrawSubTicks"=>TRUE));
 $pic->setShadow(TRUE,array("X"=>1,"Y"=>1,"R"=>0,"G"=>0,"B"=>0,"Alpha"=>10));
-$pic->drawLineChart(array("DisplayValues"=>TRUE,"DisplayColor"=>DISPLAY_AUTO,"Rounded"=>TRUE,"Surrounding"=>30));
+$pic->drawSplineChart(array("DisplayValues"=>TRUE,"DisplayColor"=>DISPLAY_AUTO,"Rounded"=>TRUE,"Surrounding"=>30));
 $pic->setShadow(FALSE);
 
 /* Render the picture */
